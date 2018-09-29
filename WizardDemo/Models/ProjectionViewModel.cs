2f42f5e3398aafa1afa3b86972d461c5e4278092
ll9 <llwierer@web.de>
2018-09-29 22:14:14 +0200

    Projection ViewModel

---
+++ WizardDemo/Models/ProjectionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardDemo.Models
+{
+    class ProjectionViewModel
+    {
+        public ProjectionViewModel(string boxText, string proj4String)
+        {
+            BoxText = boxText;
+            Proj4String = proj4String;
+        }
+
+        public string BoxText { get; set; }
+        public string Proj4String { get; set; }
+    }
+}

69f4e8cc1793917bc362baba2fc1700aa58f97c8
ll9 <llwierer@web.de>
2018-09-29 09:40:56 +0200

    IView

---
+++ WizardDemo/View/IView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardDemo.View
+{
+    interface IView
+    {
+        string ExcelPath { get; set; }
+        string XCoordinateHeader { get; set; }
+        string YCoordinateHeader { get; set; }
+        string Projection { get; set; }
+        DataTable Zuordnungstable { get; set; }
+
+        event EventHandler OnReadingExcel;
+        event EventHandler OnStoreTable;
+    }
+}

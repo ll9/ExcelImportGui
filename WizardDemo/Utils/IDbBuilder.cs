731518553a1cb097d71d2845304418b2caa8e6dc
ll9 <llwierer@web.de>
2018-09-29 13:37:23 +0200

    Created IDbBuilder

---
+++ WizardDemo/Utils/IDbBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizardDemo.Models;
+
+namespace WizardDemo.Utils
+{
+    interface IDbBuilder
+    {
+        string DbPath { get; }
+        string TableName { get; }
+        DataTable Data { get; }
+        List<ColumnInfo> ColumnInfos { get; }
+
+        void CreateTable();
+        void InsertData();
+    }
+}

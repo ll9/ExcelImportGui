16fc8ae6fe770e76bce39b24dcea6e6e7c659423
ll9 <llwierer@web.de>
2018-09-29 10:36:04 +0200

    Added Mockview

--- WizardDemo/View/IView.cs
+++ WizardDemo/View/IView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -14,9 +15,21 @@ namespace WizardDemo.View
         string XCoordinateHeader { get; set; }
         string YCoordinateHeader { get; set; }
         string Projection { get; set; }
-        List<ColumnInfo> Zuordnungstable { get; set; }
+        BindingList<ColumnInfo> Zuordnungstable { get; set; }
 
         event EventHandler OnReadingExcel;
         event EventHandler OnStoreDb;
+    }
+
+    public class MockView : IView
+    {
+        public string ExcelPath { get => @"C:\Users\Lenovo G50-45\Desktop\hugedummy.xlsx"; set => throw new NotImplementedException(); }
+        public string XCoordinateHeader { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string YCoordinateHeader { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Projection { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public BindingList<ColumnInfo> Zuordnungstable { get; set; }
+
+        public event EventHandler OnReadingExcel;
+        public event EventHandler OnStoreDb;
     }
 }
